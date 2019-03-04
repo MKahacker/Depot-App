cfd1f1dddb625fcf7014c8195c40dc560c4c832a
makk <mkahack@hawk.iit.edu>
2019-03-04 14:56:20 -0600

    Finally passing all tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,4 +9,19 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
   end
+  test "product price must be positive" do
+    product1 = Product.new(title: "French Chocolate" , description: "Chocolate from the Toulouse region of France. These rich delights will give you the rich taste of French cuisine." , image_url: "french.png")
+    product1.price = -1
+    product1.save
+    assert product1.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product1.errors[:price]
+    product1.price = 0
+    product1.save
+    assert product1.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product1.errors[:price]
+
+    product1.price = 1.0
+    product1.save
+    assert product1.valid?
+  end
 end
