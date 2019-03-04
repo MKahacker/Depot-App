ba16665872301f725f6e5e2ff0a5296845e43e25
makk <mkahack@hawk.iit.edu>
2019-03-04 15:30:39 -0600

    Changed message

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, :price, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
-  validates :title, length: {minimum: 10}
+  validates :title, length: {minimum: 10, message: "Title must be atleast ten characters long."}
   validates :image_url, allow_blank: true, format:{with: %r{.(gif|jpg|png)\Z}i, message: "must be a URL for GIF, JPG or PNG image."}
 end
