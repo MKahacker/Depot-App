fb55d2ce76b005f765496a246336b0f8f2b3951c
makk <mkahack@hawk.iit.edu>
2019-03-24 01:22:48 -0500

    Migrating   $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_060247) do
+ActiveRecord::Schema.define(version: 2019_03_24_061237) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
